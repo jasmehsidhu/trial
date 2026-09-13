5e3c4e2d6687ae71aec115060900950bb2622f95
Jasmeh Singh <142116700+jasmehsidhu@users.noreply.github.com>
2026-09-12 22:32:25 -0400

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,12 +3,23 @@ import './App.css'
 import logo from './assets/logo.png'
 
 function Navbar() {
+function open(e){
+    document.getElementById('popup').style.top='0%'
+}
+
 
   return (
     <>
+         <div id='popup'>
+            <i onClick={()=>{document.getElementById('popup').style.top='-10%'}} id='close' class="fa-solid fa-xmark" ></i>
+  <a>Home</a>
+        <a>About</a>
+        <a>Team</a>
+        <a>Contact</a>
+</div>
    <section id='navbar'>
     <img id='nlogo' src={logo}></img>
-    <div id='links'>
+<a id='ham' onClick={open}><i class="fa-solid fa-bars"></i></a>  <div id='links'>
         <a>Home</a>
         <a>About</a>
         <a>Team</a>
@@ -16,6 +27,7 @@ function Navbar() {
 
     </div>
    </section>
+
     </>
   )
 }
